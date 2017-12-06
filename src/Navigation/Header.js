02e982702be65aafb6cbdb1676bb0aca64c958f2
Melanie Martinell <melaniemartinell@Melanies-MacBook-Pro.local>
2017-12-05 16:16:47 -0800

    updated look

--- src/Navigation/Header.js
+++ src/Navigation/Header.js
@@ -6,7 +6,9 @@ class Header extends Component {
   render() {
     return (
         <div className="App-Header">   
-            <img src={logo} className="App-logo" alt="logo"/>IOT Ecosystem
+            {/* <img src={logo} className="App-logo" alt="logo"/>IOT Ecosystem */}
+            <span style={{color: '#DCD0C0', fontSize: '40px', paddingRight: '10px'}} className="App-logo fa fa-snowflake-o"> </span>
+            <span style={{paddingRight: '10px', fontSize: '25px'}}> IOT Ecosystem </span>
 
           <Nav />
         </div>
